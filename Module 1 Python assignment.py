71c0db2503d46a5a7d30b68e3d3f6595600d6f43
StrangeGaurav6 <gupta401gaurav@gmail.com>
2025-12-14 15:11:56 +0530

    Add files via upload

---
+++ Module 1 Python assignment.py
@@ -0,0 +1,23 @@
+#Task 1
+
+Num1 = float(input('Enter Number 1:'))
+Num2 = float(input('Enter Number 2:'))
+add = Num1 + Num2
+sub = Num1 - Num2
+Mul = Num1 * Num2
+Div = Num1 / Num2
+
+print('Addition :',add)
+print('Subtraction :',sub)
+print('Division :',Div)
+print('Multipication :',Mul)
+
+#Task 2
+
+FN = str(input('Enter Your Firt Name :'))
+LN = str(input('Enter Your Last Name :'))
+Name = FN +' '+ LN + '!'
+
+print('Hello,',Name,'Welcome to the python program.')
+
+         
